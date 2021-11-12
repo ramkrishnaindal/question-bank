d5896ad8dc6e7b81eac75c5035d99c08ffcd8763
ramkrishnaindal <ramkrishna_i@rediffmail.com>
2021-11-12 09:57:27 +0530

    header active class changed

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,23 +1,23 @@
 import classes from './Heading.module.css'
-import { NavLink } from 'react-router-dom'
+import { NavLink,Link } from 'react-router-dom'
 const Heading=()=>{
     return <nav className="navbar navbar-dark bg-primary navbar-expand-lg">
     <div className="container-fluid">
-      <a className={`navbar-brand ${classes["nav-link"]}`} href="#">Question Banks</a>
+      <Link to="/" className={`navbar-brand nav-link`} >Question Banks</Link>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className={`navbar-toggler-icon ${classes["nav-link"]}`}></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item">
-            <NavLink to="/" className={isActive => "nav-link" + (!isActive ? " unselected" : "")}
+            <NavLink to="/" activeClassName="active" className={`nav-link`}
    aria-current="page" href="#">Tests</NavLink>
           </li>
           <li className="nav-item">
-            <NavLink  to="/question-bank" className={isActive => "nav-link" + (!isActive ? " unselected" : "")} href="#">Question Banks</NavLink>
+            <NavLink  to="/question-bank" activeClassName="active" className={`nav-link`}>Question Banks</NavLink>
           </li>
           <li className="nav-item">
-            <NavLink  to="/create-questions"  className={isActive => "nav-link" + (!isActive ? " unselected" : "")} href="#">Create Questions</NavLink>
+            <NavLink  to="/create-questions"  activeClassName="active" className={`nav-link`}>Create Questions</NavLink>
           </li>
         </ul>
       </div>
